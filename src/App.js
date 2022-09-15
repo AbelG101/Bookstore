1fe0d1287d04520f6a0a5b8f09ae5e4a70f8bda0
AbelG101 <abelgebeyehu101@gmail.com>
2022-09-15 14:09:47 +0300

    add route components for the nav links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Category from './pages/Category';
 
 function App() {
   return (
-    <h1>Hi</h1>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Category />} />
+      </Routes>
+    </>
   );
 }
 

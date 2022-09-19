be86454b177e3e9107f67f072da2f1a86522e7f6
AbelG101 <abelgebeyehu101@gmail.com>
2022-09-20 00:33:15 +0300

    add book from redux & remove book implementation

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,34 +1,40 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
-const Book = ({ book: { title, author } }) => (
-  <div className="book">
-    <div className="book-info">
-      <div className="book-name">{title}</div>
-      <div className="author">{author}</div>
-      <div className="action-btns-container">
-        <button type="button" className="action-btn">Comments</button>
-        <div className="vertical-divider" />
-        <button type="button" className="action-btn">Remove</button>
-        <div className="vertical-divider" />
-        <button type="button" className="action-btn">Edit</button>
-      </div>
-    </div>
-    <div className="progress-info">
-      <div className="progress-amount">
-        <div className="progress-bar" />
-        <div className="progress-percent">
-          <div className="amount">64%</div>
-          <div className="completion">Completed</div>
+const Book = (props) => {
+  const { id, title, author } = props;
+  const dispatch = useDispatch();
+  return (
+    <div className="book">
+      <div className="book-info">
+        <div className="book-name">{title}</div>
+        <div className="author">{author}</div>
+        <div className="action-btns-container">
+          <button type="button" className="action-btn comment">Comments</button>
+          <div className="vertical-divider" />
+          <button onClick={() => (dispatch(removeBook((id))))} type="button" className="action-btn remove">Remove</button>
+          <div className="vertical-divider" />
+          <button type="button" className="action-btn edit">Edit</button>
         </div>
       </div>
-      <div className="progress-divider" />
-      <div className="chapter-info">
-        <div className="heading">CURRENT CHAPTER</div>
-        <div className="current-chapter">Chapter 17</div>
-        <button type="button" className="update-progress-btn">UPDATE PROGRESS</button>
+      <div className="progress-info">
+        <div className="progress-amount">
+          <div className="progress-bar" />
+          <div className="progress-percent">
+            <div className="amount">64%</div>
+            <div className="completion">Completed</div>
+          </div>
+        </div>
+        <div className="progress-divider" />
+        <div className="chapter-info">
+          <div className="heading">CURRENT CHAPTER</div>
+          <div className="current-chapter">Chapter 17</div>
+          <button type="button" className="update-progress-btn">UPDATE PROGRESS</button>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Book;
